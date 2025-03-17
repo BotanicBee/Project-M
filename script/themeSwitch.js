ca9b2ec43d7993908688f54bef73f5d15cb77e65
BotanicBee <mariaanotto77@gmail.com>
2025-03-17 20:05:13 +0200

    Skills page & Cleanup
    Skills page description needs work, game to follow.

--- script/themeSwitch.js
+++ script/themeSwitch.js
@@ -1,20 +1,20 @@
 
 /*I want the local storage theme to have preference, but if there is nothing set, I want the code to check the PC settings if the user does have a Dark or Light mode set. I want secondary theme option to come from here.*/
 const rootElm = document.documentElement;
-rootElm.setAttribute('data-theme', localStorage.getItem('theme'));
+rootElm.setAttribute('data-theme',  localStorage.getItem('theme'));
 
 const switchTheme = () => {
-    //fetching root elements and data-theme from css
+//fetching root elements and data-theme from css
 
     const rootElm = document.documentElement; /*rootElm is documentElement*/
     let dataTheme = rootElm.getAttribute('data-theme');
 
     const newTheme = (dataTheme === 'pastel') ? 'dark' : 'pastel'; /*usage of ? and : here- the ? means if the fist condition after the ? is true, activate, if not, activate ssecond condition.*/
 
-    /*set atribute to newTheme (atribute=value pairs)*/
+/*set atribute to newTheme (atribute=value pairs)*/
     rootElm.setAttribute('data-theme', newTheme);
 
-    /*I want the browser to remember the theme when the page is reloaded. Ill use the local storage for this.*/
+/*I want the browser to remember the theme when the page is reloaded. Ill use the local storage for this.*/
     localStorage.setItem('theme', newTheme);
 }
 
